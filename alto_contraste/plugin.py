ab6a0cfad6620986fc26f85967a84935a60d47f8
Lucas Araçá <121361767+Satonix@users.noreply.github.com>
2026-01-16 16:13:49 -0300

    Update plugin.py

--- alto_contraste/plugin.py
+++ alto_contraste/plugin.py
@@ -15,17 +15,16 @@ class AltoContrastePlugin(VisualPlugin):
     def style_table_cell(self, entry, column: str):
         if entry.status == TranslationStatus.IN_PROGRESS:
             return TextStyle(
-                background="#747812",
-                color="#ffffff"
+                background="#747812"
             )
 
         if entry.status == TranslationStatus.TRANSLATED:
             return TextStyle(
-                background="#2A4F31",
-                color="#ffffff"
+                background="#2A4F31"
             )
 
         return None
+
 
 
 
